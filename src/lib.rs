b4ad06311bd2161e9eee583066dddab079e1ad33
Hans Larsson <hanandlia@gmail.com>
2024-01-30 09:45:43 -0600

    start using underscores in function names

--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,18 @@ impl Searcher {
             searchstring: String::new(),
         }
     }
-    pub fn searchresults(&self) -> Result<Vec<String>, String> {
-        let filteredlist: Vec<String> = self.substringsearch(&self.searchstring);
+    pub fn search_results(&self) -> Result<Vec<String>, String> {
+        let filteredlist: Vec<String> = self.substring_search(&self.searchstring);
         if filteredlist.is_empty() {
             Err(String::from("String not found"))
         } else {
             Ok(filteredlist)
         }
     }
-    pub fn addsearch(&mut self, character: char) {
+    pub fn add_search(&mut self, character: char) {
         self.searchstring.push(character)
     }
-    fn substringsearch(&self, searchstring: &str) -> Vec<String> {
+    fn substring_search(&self, searchstring: &str) -> Vec<String> {
         self.tobesearched
             .clone()
             .into_iter()
@@ -35,10 +35,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn two_word_test() {
         let mut testsearcher = Searcher::new(vec![String::from("hi"), String::from("hill")]);
-        testsearcher.addsearch('h');
-        testsearcher.addsearch('i');
-        assert_eq!(testsearcher.searchresults().unwrap().len(), 2);
+        testsearcher.add_search('h');
+        testsearcher.add_search('i');
+        assert_eq!(testsearcher.search_results().unwrap().len(), 2);
     }
 }
